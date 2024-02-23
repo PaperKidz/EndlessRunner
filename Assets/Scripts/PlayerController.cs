011b0bb6a00ef3ad3a9e2b776c5ea3c7c8f99b2a
Dragonwarriorg <70964249+PaperKidz@users.noreply.github.com>
2024-02-23 09:13:33 +0530

    new commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+
+    float hinput;
+    float vinput;
+    Vector3 Movement;
+
+    [SerializeField] 
+    float speed=7f;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("tilak didi is wild");
+       rb=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       PlayerInput();
+       
+       
+       
     }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void PlayerInput()
+    {
+        hinput=Input.GetAxis("Horizontal");
+        vinput=Input.GetAxis("Vertical");
+        Movement= new Vector3(hinput,0,vinput);
+    }
+
+    void Move()
+    {
+           
+        rb.AddForce(Movement*speed);
+        Debug.Log(Movement*speed);
+    }
+    
 }
